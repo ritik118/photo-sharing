070e4b18d30c40573df34ed7d1fad06cc0c3ffa4
ritik118 <ritikkumar6081@gmail.com>
2022-01-04 01:03:31 +0530

    Frontend In Progress day 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router , Route  } from 'react-router-dom';
+import { BrowserRouter as Router , Route, Redirect, Switch  } from 'react-router-dom';
+import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 import Users from './user/pages/Users';
 
 const App = () => {
-  return <Router>
-    <Route path="/">
+  return (<Router>
+    <MainNavigation />
+    <main>
+    <Switch>
+    <Route path="/" exact>
       <Users />
     </Route>
-  </Router>;
+    <Route path="/places/new" exact>
+      <NewPlace />
+    </Route>
+    <Route path="/:userId/places" exact>
+      <UserPlaces />
+    </Route>
+    <Redirect to="/" />
+    </Switch>
+    </main>
+  </Router>);
 }
 
 export default App;
